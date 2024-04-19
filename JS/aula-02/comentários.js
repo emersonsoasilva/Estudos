317f9d7151ef94d0bce4b711e328889f75f65a82
TwoHundredFaces <emersonsoares141@gmail.com>
2024-04-19 15:30:11 -0300

    first commit

---
+++ JS/aula-02/comentários.js
@@ -0,0 +1,20 @@
+// E escrever um comentário longo
+console.log('Olá, mundo!'); // Aqui tem um comentário
+
+console.log('Linha 1');
+
+//Vai ser ignorado
+// console.log('Linha 2');
+
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+
+// Comentário de linha (Bloco)
+
+/**
+ * Um título
+ * 
+ * Lorem Ipsum
+ */
